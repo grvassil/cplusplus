75326aee17a3cf018b59d328e91cb72ad3c9251c
grvassil <grvassil@gmail.com>
2021-12-10 20:40:00 +0200

    exerc. 12

---
+++ greg12.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char a[6];
+    int i, j;
+    for (i = 0; i < 6; i++)
+    {
+        cin >> a[i];
+    }
+
+    bool flag = false;
+    i = 0;
+    j = 0;
+    while (!flag && (a[i] >= 'a' && a[i] <= 'z') || (a[i] >='A' && a[i] <= 'Z'))
+    {
+        /* code */
+    }
+    
+    
+
+    // int n = 0, c;
+    // c = getchar();
+    // while (c!= EOF)
+    // {
+    //     n++;
+    //     c = getchar();
+    // }
+
+    // cout << c;
+    
+}
